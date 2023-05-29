b8a15de6a92cacdca967b36025f00c0af9632879
Spencer Beckwith <spencerjbeckwith@gmail.com>
2023-05-29 03:35:11 -0700

    now saves output chr

--- main.go
+++ main.go
@@ -11,31 +11,75 @@ import (
 
 func main() {
 	start := time.Now()
-	input, output, _ := readCmd()
+	input, output, mode := readCmd()
 
-	// read input file
+	// Read input file
 	data, err := os.Open(input)
 	if err != nil {
-		fmt.Println(input, "- unable to open input:", err)
+		fmt.Printf("%s - unable to open input: %s\n", input, err)
 		os.Exit(2)
 	}
 
-	// decode the data
+	// Decode the data
 	img, _, err := image.Decode(data)
 	if err != nil {
-		fmt.Println(input, "- unable to decode input:", err)
+		fmt.Printf("%s - unable to decode input: %s\n", input, err)
 		os.Exit(3)
 	}
 
-	// initialize list of colors
+	// Determine width/height and make sure they're valid
+	bounds := img.Bounds()
+	width := bounds.Max.X
+	height := bounds.Max.Y
+	if width%8 != 0 || height%8 != 0 {
+		fmt.Printf("%s - image size must be a multiple of 8. Got %d x %d\n", input, width, height)
+		os.Exit(4)
+	}
+
+	total := (width * height) / 64
+	var buffer []byte
+
+	// Initialize list of colors
 	colors := detectColors(img)
 
-	convertTile(img, colors, 0, 0)
+	// Scan each tile of the image
+	if mode == "horizontal" {
+	outH:
+		for y := 0; y < height; y += 8 {
+			for x := 0; x < width; x += 8 {
+				data, err2 := convertTile(img, colors, x, y)
+				if err2 != nil {
+					err = err2
+					break outH
+				}
+				buffer = append(buffer, data[:]...)
+			}
+		}
+	} else if mode == "vertical" {
+	outV:
+		for x := 0; x < width; x += 8 {
+			for y := 0; y < height; y += 8 {
+				data, err2 := convertTile(img, colors, x, y)
+				if err2 != nil {
+					err = err2
+					break outV
+				}
+				buffer = append(buffer, data[:]...)
+			}
+		}
+	}
 
-	// TODO initialze output buffer
-	// TODO figure out color/luminosity detection
-	// TODO read tiles in correct order
-	// TODO write to output file
+	if err != nil {
+		fmt.Printf("%s - unable to convert: %s\n", input, err)
+		os.Exit(5)
+	}
 
-	fmt.Printf("%s -> %s (%s)\n", input, output, time.Since(start))
+	// Write the output file
+	err = os.WriteFile(output, buffer, 0666)
+	if err != nil {
+		fmt.Printf("%s - unable to save %s: %s", input, output, err)
+		os.Exit(6)
+	}
+
+	fmt.Printf("%s -> %s (%d tiles in %s)\n", input, output, total, time.Since(start))
 }
